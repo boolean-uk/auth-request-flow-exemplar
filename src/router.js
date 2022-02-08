0cd063762a6ae0e98839d5d8cace0df8bd99d5a2
Nathan King <nathanking3590@gmail.com>
2022-02-08 12:22:18 +0000

    Add solution

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
+const jwtSecret = 'mysecretkey';
 const mockUser = {
     username: 'authguy',
     password: 'mypassword',
@@ -14,11 +15,26 @@ const mockUser = {
 };
 
 router.post('/login', (req, res) => {
+    const { username, password } = req.body;
 
+    if (username !== mockUser.username || password !== mockUser.password) {
+        return res.status(401).json({ error: 'Invalid username or password.' });
+    }
+
+    const token = jwt.sign({ username }, jwtSecret);
+
+    res.json({ data: token });
 });
 
 router.get('/profile', (req, res) => {
+    try {
+        const token = req.header('authorization');
+        jwt.verify(token, jwtSecret);
+    } catch (error) {
+        return res.status(401).json({ error: 'Invalid token provided.' });
+    }
 
+    res.json({ data: mockUser.profile });
 });
 
 module.exports = router;
