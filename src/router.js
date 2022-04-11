91aba48f5e14181e9f3bb32d57b26a6e1e4e8643
phillipdunn <phillipdunn@hotmail.com>
2022-04-11 15:51:57 +0100

    add - requirement 1

--- src/router.js
+++ src/router.js
@@ -4,21 +4,21 @@ const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 const mockUser = {
-    username: 'authguy',
-    password: 'mypassword',
-    profile: {
-        firstName: 'Chris',
-        lastName: 'Wolstenholme',
-        age: 43
-    }
+  username: 'authguy',
+  password: 'mypassword',
+  profile: {
+    firstName: 'Chris',
+    lastName: 'Wolstenholme',
+    age: 43,
+  },
 };
 
+const secretKey = 'mysecretkey';
+
 router.post('/login', (req, res) => {
-
-});
-
-router.get('/profile', (req, res) => {
-
+  const payload = mockUser.username;
+  const token = jwt.sign(payload, secretKey);
+  return res.json({ token });
 });
 
 module.exports = router;
