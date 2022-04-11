fa1466dace8505cb506f62459f3bf313021da79e
Lewis <ljoncb@gmail.com>
2022-04-11 15:36:27 +0100

    part 2

--- src/router.js
+++ src/router.js
@@ -13,12 +13,24 @@ const mockUser = {
     }
 };
 
-router.post('/login', (req, res) => {
+const jwtSecret = 'super-secret'
 
+router.post('/login', (req, res) => {
+    const payload = {username: mockUser.username}
+    const token = jwt.sign(payload , jwtSecret)
+    res.status(200).json({token: token})
 });
 
 router.get('/profile', (req, res) => {
-
+    try{
+        let token = req.headers.authorization
+        token = token.replace('Bearer ', '')
+        jwt.verify(token, jwtSecret)
+        res.json({ profile: mockUser.profile });
+    }catch(err) {
+        console.error(err)
+        return res.status(401).json({error: 'unauthorized'})
+    }
 });
 
 module.exports = router;
